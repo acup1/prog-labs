ff2c2546aae7ba97269e73a592a6deb0dc165cb0
A cup <georgiyegoriy@gmail.com>
2025-04-17 15:11:41 +0300

    qwe

---
+++ 2/main.cpp
@@ -0,0 +1,25 @@
+#include "func.cpp"
+#include "func.h"
+
+int main() {
+  // инициализация переменных
+  int **matr;
+  int N;
+
+  // решение задач для первой матрицы
+  error(defMatr("matr_A.txt", matr, N));
+  printMatr("A", matr, N); // эхо-печать матрицы
+  error(task1(matr, N));
+  error(task2(matr, N));
+
+  // удаление матрицы
+  delMatr(matr, N);
+
+  // решение задач для второй матрицы
+  error(defMatr("matr_B.txt", matr, N));
+  printMatr("B", matr, N); // эхо-печать матрицы
+  error(task1(matr, N));
+  error(task2(matr, N));
+
+  return 0;
+}
